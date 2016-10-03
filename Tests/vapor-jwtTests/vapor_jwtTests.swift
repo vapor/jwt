41a6f6074c9509ff02c9034b89a0e40b1ff8769a
Siemen Sikkema <siemen@unclutter.nl>
2016-10-03 21:32:45 +0200

    Add es384/es512 encryption + es verification

--- Tests/vapor-jwtTests/vapor_jwtTests.swift
+++ Tests/vapor-jwtTests/vapor_jwtTests.swift
@@ -41,27 +41,10 @@ func decode(_ jwt: String) throws -> JSON {
     return try JSON(bytes: try data.makeBytes())
 }
 
-enum Algorithm {
-    case none
-    case hs256(String)
-    case hs384(String)
-    case hs512(String)
-//    case rs256(String)
-//    case rs384(String)
-//    case rs512(String)
-    case es256(String)
-//    case es384(String)
-//    case es512(String)
-}
-
-//let privateKeyBytes: Bytes = [0x8e,0xbc,0xd1,0x89,0x6b,0x3a,0x83,0x47,0x46,0x1d,0x24,0x57,0xaa,0x11,0xe2,0xd0,0xe4,0x1e,0x8e,0x10,0xf3,0xdb,0xf4,0xe9,0x49,0xde,0x25,0xa9,0xe4,0x0d,0x2c,0xa6]
-let privateKey = "jrzRiWs6g0dGHSRXqhHi0OQejhDz2/TpSd4lqeQNLKY="
-
-func newKeyPair(privateBase64Key: String) throws -> OpaquePointer {
+func newECKeyPair(_ hashSize: HashSize) throws -> OpaquePointer {
     guard
-        let ecKey = EC_KEY_new_by_curve_name(NID_X9_62_prime256v1),
-        let privateBytes = try Data(base64Encoded: privateBase64Key)?.makeBytes(),
-        privateBytes.count == 32 else {
+        let ecKey = EC_KEY_new_by_curve_name(hashSize.curve),
+        let privateBytes = try Data(base64Encoded: hashSize.key)?.makeBytes() else {
             throw JWTError.couldNotGenerateKey
     }
     var privateNum = BIGNUM()
@@ -69,7 +52,7 @@ func newKeyPair(privateBase64Key: String) throws -> OpaquePointer {
     // Set private key
 
     BN_init(&privateNum)
-    BN_bin2bn(privateBytes, 32, &privateNum)
+    BN_bin2bn(privateBytes, Int32(privateBytes.count), &privateNum)
     EC_KEY_set_private_key(ecKey, &privateNum)
 
     // Derive public key
@@ -110,26 +93,93 @@ func newKeyPair(privateBase64Key: String) throws -> OpaquePointer {
     BN_CTX_end(context)
     BN_CTX_free(context)
     BN_clear_free(&privateNum)
-
+    
     return ecKey
+}
+
+func newECPublicKey(_ hashSize: HashSize) throws -> OpaquePointer {
+    guard let publicBytes = try Data(base64Encoded: hashSize.key)?.makeBytes() else {
+        throw JWTError.couldNotGenerateKey
+    }
+
+    var ecKey = EC_KEY_new_by_curve_name(hashSize.curve)
+    var publicBytesPointer: UnsafePointer? = UnsafePointer<UInt8>(publicBytes)
+
+    if let ecKey = o2i_ECPublicKey(&ecKey, &publicBytesPointer, publicBytes.count) {
+        return ecKey
+    } else {
+        throw JWTError.couldNotGenerateKey
+    }
+}
+
+enum HashSize {
+    case _256(String)
+    case _384(String)
+    case _512(String)
+}
+
+extension HashSize {
+
+    var curve: Int32 {
+        switch self {
+        case ._256: return NID_secp256k1
+        case ._384: return NID_secp384r1
+        case ._512: return NID_secp521r1
+        }
+    }
+
+    var key: String {
+        switch self {
+        case ._256(let key), ._384(let key), ._512(let key):
+            return key
+        }
+    }
+
+    var shaHashMethod: Hash.Method {
+        switch self {
+        case ._256: return .sha256
+        case ._384: return .sha384
+        case ._512: return .sha512
+        }
+    }
+
+    var shaHMACMethod: HMAC.Method {
+        switch self {
+        case ._256: return .sha256
+        case ._384: return .sha384
+        case ._512: return .sha512
+        }
+    }
+
+    var string: String {
+        switch self {
+        case ._256: return "256"
+        case ._384: return "384"
+        case ._512: return "512"
+        }
+    }
+}
+
+enum Algorithm {
+    case none
+    case hs(HashSize)
+//    case rs(HashSize)
+    case es(HashSize)
 }
 
 extension Algorithm {
 
     func encrypt(_ string: String) throws -> String {
-
-        if case .es256(let privateKey) = self {
-            var digest = try Hash(.sha256, string.bytes).hash()
-
-            let ecKey = try newKeyPair(privateBase64Key: privateKey)
-
+        switch self {
+        case .hs(let hashSize):
+            return try HMAC(hashSize.shaHMACMethod, string.bytes).authenticate(key: hashSize.key.bytes).base64String
+        case .es(let hashSize):
+            var digest = try Hash(hashSize.shaHashMethod, string.bytes).hash()
             let digestLength = Int32(digest.count)
 
-            guard let sig = ECDSA_do_sign(&digest, digestLength, ecKey) else {
-                throw JWTError.couldNotGenerateKey
-            }
+            let ecKey = try newECKeyPair(hashSize)
 
-            guard ECDSA_do_verify(&digest, digestLength, sig, ecKey) == 1 else {
+            guard let sig = ECDSA_do_sign(&digest, digestLength, ecKey) else {
                 throw JWTError.couldNotGenerateKey
             }
 
@@ -147,96 +197,108 @@ extension Algorithm {
             }
 
             return bytes.base64String
-        } else if let method = method, let signKey = signKey {
-            return try HMAC(method, string.bytes).authenticate(key: signKey.bytes).base64String
-        }
-
-        return ""
-    }
-
-    var signKey: String? {
-        switch self {
-        case .hs256(let key), .hs384(let key), .hs512(let key):
-            return key
         default:
-            return nil
+            return ""
         }
     }
 
     var headerValue: String {
         switch self {
         case .none: return "none"
-        case .hs256: return "HS256"
-        case .hs384: return "HS384"
-        case .hs512: return "HS512"
-//        case .rs256: return "RS256"
-//        case .rs384: return "RS384"
-//        case .rs512: return "RS512"
-        case .es256: return "ES256"
-//        case .es384: return "ES384"
-//        case .es512: return "ES512"
+        case .hs(let hashSize):
+            return "HS" + hashSize.string
+        case .es(let hashSize):
+            return "ES" + hashSize.string
         }
-    }
-
-    private var method: HMAC.Method? {
-        switch self {
-        case .hs256: return .sha256
-        case .hs384: return .sha384
-        case .hs512: return .sha512
-//        case .rs256: return
-//        case .rs384: return
-//        case .rs512: return
-        default: return nil
-        }
-    }
-}
-
-extension Algorithm {
-    static var all: [Algorithm] {
-        return [
-            .hs256(""),
-            .hs384(""),
-            .hs512(""),
-//            .rs256(""),
-//            .rs384(""),
-//            .rs512(""),
-            .es256(""),
-//            .es384(""),
-//            .es512("")
-
-        ]
     }
 }
 
 class vapor_jwtTests: XCTestCase {
 
+    let message = JSON(["a": .string("b")])
+
     func testEncode() {
-        XCTAssertEqual(try encode(JSON(["a": .string("b")]), with: .none),
+        XCTAssertEqual(try encode(message, with: .none),
                        "eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0=.eyJhIjoiYiJ9.")
     }
 
     func testEncodeWithHS256Encryption() {
-        XCTAssertEqual(try encode(JSON(["a": .string("b")]), with: .hs256("secret")),
+        XCTAssertEqual(try encode(message, with: .hs(._256("secret"))),
                        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhIjoiYiJ9.jiMyrsmD8AoHWeQgmxZ5yq8z0lXS67/QGs52AzC8Ru8=")
     }
 
     func testEncodeWithHS384Encryption() {
-        XCTAssertEqual(try encode(JSON(["a": .string("b")]), with: .hs384("secret")),
+        XCTAssertEqual(try encode(message, with: .hs(._384("secret"))),
                        "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJhIjoiYiJ9.TgJIRKof/BR1EVkcRXV/xt8IxffTwQKUyOZIUUWFrC6sFAGIDe1HZksCbSIhKyoD")
     }
 
     func testEncodeWithHS512Encryption() {
-        XCTAssertEqual(try encode(JSON(["a": .string("b")]), with: .hs512("secret")),
+        XCTAssertEqual(try encode(message, with: .hs(._512("secret"))),
                        "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhIjoiYiJ9.NEvauwy77uvgEvKOlGLmLJJEseamUKhAAPaGaWlD5P5qLHkLiHC9eQOy4YwR+L3BjNN1lumBhg8eEHus23CflQ==")
     }
 
     func testEncodeWithES256Encryption() {
-        XCTAssertEqual(try encode(JSON(["a": .string("b")]), with: .es256(privateKey)),
-                       "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJhIjoiYiJ9.MEUCIElt+2fI1t5Lj/EcVho2JUSpw0Pl64GdNOpKXVzEeWZcAiEAnmZ0uCdOVdKdVnAqfBsbiETNiToWfgZNYaSsVL7wibA=")
+        let privateKey = "AL3BRa7llckPgUw3Si2KCy1kRUZJ/pxJ29nlr86xlm0="
+        let publicKey = "BPtT5aOJu133UmfZNr6J0xYifrtknN0sk0VbuB/xqdQHCFpzpWmm8c9HnesXRvu21o37MkzkO6hKxGFNzO73UGc="
+
+        let token = try! encode(message, with: .es(._256(privateKey)))
+        let segments = token.components(separatedBy: ".")
+
+        XCTAssertEqual(segments.count, 3)
+        let derBytes = try! Data(base64Encoded: segments[2])!.makeBytes()
+        var derBytesPointer: UnsafePointer? = UnsafePointer(derBytes)
+
+        let signature = d2i_ECDSA_SIG(nil, &derBytesPointer, derBytes.count)
+
+        let digest = try! Hash(.sha256, "\(segments[0]).\(segments[1])".bytes).hash()
+        let ecKey = try! newECPublicKey(._256(publicKey))
+        let verified = ECDSA_do_verify(digest, Int32(digest.count), signature, ecKey)
+
+        XCTAssertEqual(verified, 1)
+    }
+
+    func testEncodeWithES384Encryption() {
+        let privateKey = "BVjgIwqB1PeIv0YP3ZeQ7CTqW+9+yJ7Y/jXPr1tfrb3ne5TQNMU2rOxYtf6M6IN9"
+        let publicKey = "BAV9NUl1v488eXcDhStPTK57Dg4Cm+XCjmFGD1IXjkF+LJNG963oUkYjV1xxNQgxJka7tsXjadei25PdZatX3GS6KivPiNu1YdeSpXxr5d73sdLU/rq/OgDLUUcic77g3A=="
+
+        let token = try! encode(message, with: .es(._384(privateKey)))
+        let segments = token.components(separatedBy: ".")
+
+        XCTAssertEqual(segments.count, 3)
+        let derBytes = try! Data(base64Encoded: segments[2])!.makeBytes()
+        var derBytesPointer: UnsafePointer? = UnsafePointer(derBytes)
+
+        let signature = d2i_ECDSA_SIG(nil, &derBytesPointer, derBytes.count)
+
+        let digest = try! Hash(.sha384, "\(segments[0]).\(segments[1])".bytes).hash()
+        let ecKey = try! newECPublicKey(._384(publicKey))
+        let verified = ECDSA_do_verify(digest, Int32(digest.count), signature, ecKey)
+
+        XCTAssertEqual(verified, 1)
+    }
+
+    func testEncodeWithES512Encryption() {
+        let privateKey = "AdcqLUGC+4OlxqGRJkz6++BD9tfDtNM1NvTQh7M0stzvMMvcaNio87YUD6Gaks0eS9Krvs1Bkqo/T7k9DW8Eyoec"
+        let publicKey = "BACJKkoouMqhbgZtTSyQJHECDf/V2ArN7VwuaIUIKsx/OFiF79ccrzCSZ1MrGOmQgPAez6pqjUIjhwoHr5tRH65BggEUhC7SNvRvfMeElOrZNac9uTVnfGJ1DywnL+JtD49ytuD9GjifUPHJi4RcN36RHLXyBpF0u1+RRFbKCNhnJ132Pw=="
+
+        let token = try! encode(message, with: .es(._512(privateKey)))
+        let segments = token.components(separatedBy: ".")
+
+        XCTAssertEqual(segments.count, 3)
+        let derBytes = try! Data(base64Encoded: segments[2])!.makeBytes()
+        var derBytesPointer: UnsafePointer? = UnsafePointer(derBytes)
+
+        let signature = d2i_ECDSA_SIG(nil, &derBytesPointer, derBytes.count)
+
+        let digest = try! Hash(.sha512, "\(segments[0]).\(segments[1])".bytes).hash()
+        let ecKey = try! newECPublicKey(._512(publicKey))
+        let verified = ECDSA_do_verify(digest, Int32(digest.count), signature, ecKey)
+
+        XCTAssertEqual(verified, 1)
     }
 
     func testDecode() {
-        XCTAssertEqual(try decode("eyJhIjoiYiJ9"), JSON(["a": .string("b")]))
+        XCTAssertEqual(try decode("eyJhIjoiYiJ9"), message)
     }
 
     func testNotBase64Encoded() {
@@ -250,17 +312,23 @@ class vapor_jwtTests: XCTestCase {
     }
 
     func testHeaderKeys() {
-        XCTAssertEqual(
-            Algorithm.all.map { $0.headerValue }, [
-                "HS256",
-                "HS384",
-                "HS512",
-//                "RS256",
-//                "RS384",
-//                "RS512",
-                "ES256",
-//                "ES384",
-//                "ES512"
-            ])
+        let all: [Algorithm] = [
+            .none,
+            .hs(._256("")),
+            .hs(._384("")),
+            .hs(._512("")),
+            .es(._256("")),
+            .es(._384("")),
+            .es(._512(""))
+        ]
+        XCTAssertEqual(all.map { $0.headerValue }, [
+            "none",
+            "HS256",
+            "HS384",
+            "HS512",
+            "ES256",
+            "ES384",
+            "ES512"
+        ])
     }
 }

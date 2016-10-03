deb3e4f9d211b3795a7c9962972b19e23aed81ae
Siemen Sikkema <siemen@unclutter.nl>
2016-10-03 23:47:33 +0200

    Refactor to JWToken

--- Tests/vapor-jwtTests/vapor_jwtTests.swift
+++ Tests/vapor-jwtTests/vapor_jwtTests.swift
@@ -7,30 +7,76 @@ import HMAC
 import JSON
 import XCTest
 
-enum Header: String {
-    case algorithm = "alg"
-    case type = "typ"
-}
-
 enum JWTError: Error {
-    case notBase64Encoded
     case couldNotGenerateKey
+    case incorrectNumberOfSegments
+    case missingAlgorithm
+    case notBase64Encoded
+    case unsupportedAlgorithm
 }
 
 extension JSON {
+
+    init(base64Encoded: String) throws {
+        guard let data = Data(base64Encoded: base64Encoded) else {
+            throw JWTError.notBase64Encoded
+        }
+
+        try self.init(bytes: try data.makeBytes())
+    }
+    
     func base64String() throws -> String {
         return try makeBytes().base64String
     }
 }
 
-func encode(_ payload: JSON, with algorithm: Algorithm) throws -> String {
-    let header = JSON([Header.algorithm.rawValue: .string(algorithm.headerValue),
+struct JWToken {
+
+    enum Header: String {
+        case algorithm = "alg"
+        case type = "typ"
+    }
+
+    private let algorithmHeaderValue: String
+    let header: JSON
+    let payload: JSON
+    let signature: String
+
+    // TODO: support extra header fields
+    init(payload: JSON, algorithm: Algorithm) throws {
+        header = JSON([Header.algorithm.rawValue: .string(algorithm.headerValue),
                        Header.type.rawValue: "JWT"])
+        self.algorithmHeaderValue = algorithm.headerValue
+        self.payload = payload
+        let encodedHeaderAndPayload = "\(try header.base64String()).\(try payload.base64String())"
+        signature = try algorithm.encrypt(encodedHeaderAndPayload)
+    }
 
-    let encodedHeaderAndPayload = "\(try header.base64String()).\(try payload.base64String())"
-    let signature = try algorithm.encrypt(encodedHeaderAndPayload)
+    init(token: String) throws {
+        let segments = token.components(separatedBy: ".")
+        guard segments.count == 3 else {
+            throw JWTError.incorrectNumberOfSegments
+        }
+        header = try JSON(base64Encoded: segments[0])
+        guard let alg = header.object?[Header.algorithm.rawValue]?.string else {
+            throw JWTError.missingAlgorithm
+        }
+        algorithmHeaderValue = alg
+        payload = try JSON(base64Encoded: segments[1])
+        signature = segments[2]
+    }
 
-    return "\(encodedHeaderAndPayload).\(signature)"
+    func tokenString() throws -> String {
+        return "\(try header.base64String()).\(try payload.base64String()).\(signature)"
+    }
+
+    func verifySignature(key: String) throws -> Bool {
+        let algorithm = try Algorithm(algorithmHeaderValue, key: key)
+
+        let encodedHeaderAndPayload = "\(try header.base64String()).\(try payload.base64String())"
+
+        return try algorithm.verifySignature(signature, message: encodedHeaderAndPayload)
+    }
 }
 
 func decode(_ jwt: String) throws -> JSON {
@@ -120,6 +166,16 @@ enum HashSize {
 
 extension HashSize {
 
+    init(_ string: String, key: String) throws {
+        switch string {
+        case "256": self = ._256(key)
+        case "384": self = ._384(key)
+        case "512": self = ._512(key)
+        default:
+            throw JWTError.unsupportedAlgorithm
+        }
+    }
+
     var curve: Int32 {
         switch self {
         case ._256: return NID_secp256k1
@@ -162,24 +218,42 @@ extension HashSize {
 
 enum Algorithm {
     case none
+    case es(HashSize)
     case hs(HashSize)
 //    case rs(HashSize)
-    case es(HashSize)
 }
 
 extension Algorithm {
 
-    func encrypt(_ string: String) throws -> String {
+    init(_ string: String, key: String) throws {
+        guard string != "none" else {
+            self = .none
+            return
+        }
+
+        let hashSizeRange = string.index(string.startIndex, offsetBy: 2)..<string.endIndex
+        let hashSize = try HashSize(string[hashSizeRange], key: key)
+
+        switch string.substring(to: string.index(string.startIndex, offsetBy: 2)).lowercased() {
+        case "es":
+            self = .es(hashSize)
+        case "hs":
+            self = .hs(hashSize)
+        default:
+            throw JWTError.unsupportedAlgorithm
+        }
+    }
+
+    func encrypt(_ message: String) throws -> String {
         switch self {
         case .hs(let hashSize):
-            return try HMAC(hashSize.shaHMACMethod, string.bytes).authenticate(key: hashSize.key.bytes).base64String
+            return try HMAC(hashSize.shaHMACMethod, message.bytes)
+                .authenticate(key: hashSize.key.bytes).base64String
         case .es(let hashSize):
-            var digest = try Hash(hashSize.shaHashMethod, string.bytes).hash()
-            let digestLength = Int32(digest.count)
-
+            var digest = try Hash(hashSize.shaHashMethod, message.bytes).hash()
             let ecKey = try newECKeyPair(hashSize)
 
-            guard let sig = ECDSA_do_sign(&digest, digestLength, ecKey) else {
+            guard let sig = ECDSA_do_sign(&digest, Int32(digest.count), ecKey) else {
                 throw JWTError.couldNotGenerateKey
             }
 
@@ -202,6 +276,25 @@ extension Algorithm {
         }
     }
 
+    func verifySignature(_ signature: String, message: String) throws -> Bool {
+        switch self {
+        case .none: return true
+        case .hs:
+            return try encrypt(message) == signature
+        case .es(let hashSize):
+            guard let der = Data(base64Encoded: signature) else {
+                throw JWTError.notBase64Encoded
+            }
+            let derBytes = try der.makeBytes()
+            var derBytesPointer: UnsafePointer? = UnsafePointer(derBytes)
+            let signature = d2i_ECDSA_SIG(nil, &derBytesPointer, derBytes.count)
+            let digest = try Hash(hashSize.shaHashMethod, message.bytes).hash()
+            let ecKey = try newECPublicKey(hashSize)
+            let verified = ECDSA_do_verify(digest, Int32(digest.count), signature, ecKey)
+            return verified == 1
+        }
+    }
+
     var headerValue: String {
         switch self {
         case .none: return "none"
@@ -217,84 +310,84 @@ class vapor_jwtTests: XCTestCase {
 
     let message = JSON(["a": .string("b")])
 
-    func testEncode() {
-        XCTAssertEqual(try encode(message, with: .none),
-                       "eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0=.eyJhIjoiYiJ9.")
+    func testEncodeWithoutEncryption() {
+        do {
+            let token = try JWToken(payload: message, algorithm: .none)
+            XCTAssertEqual(try token.tokenString(),
+                           "eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0=.eyJhIjoiYiJ9.")
+            XCTAssertTrue(try token.verifySignature(key: ""))
+        } catch {
+            XCTFail()
+        }
     }
 
     func testEncodeWithHS256Encryption() {
-        XCTAssertEqual(try encode(message, with: .hs(._256("secret"))),
-                       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhIjoiYiJ9.jiMyrsmD8AoHWeQgmxZ5yq8z0lXS67/QGs52AzC8Ru8=")
+        do {
+            let token = try JWToken(payload: message, algorithm: .hs(._256("secret")))
+            XCTAssertEqual(try token.tokenString(),
+                           "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhIjoiYiJ9.jiMyrsmD8AoHWeQgmxZ5yq8z0lXS67/QGs52AzC8Ru8=")
+            XCTAssertTrue(try token.verifySignature(key: "secret"))
+        } catch {
+            XCTFail()
+        }
     }
 
     func testEncodeWithHS384Encryption() {
-        XCTAssertEqual(try encode(message, with: .hs(._384("secret"))),
-                       "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJhIjoiYiJ9.TgJIRKof/BR1EVkcRXV/xt8IxffTwQKUyOZIUUWFrC6sFAGIDe1HZksCbSIhKyoD")
+        do {
+            let token = try JWToken(payload: message, algorithm: .hs(._384("secret")))
+            XCTAssertEqual(try token.tokenString(),
+                           "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJhIjoiYiJ9.TgJIRKof/BR1EVkcRXV/xt8IxffTwQKUyOZIUUWFrC6sFAGIDe1HZksCbSIhKyoD")
+            XCTAssertTrue(try token.verifySignature(key: "secret"))
+        } catch {
+            XCTFail()
+        }
     }
 
     func testEncodeWithHS512Encryption() {
-        XCTAssertEqual(try encode(message, with: .hs(._512("secret"))),
-                       "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhIjoiYiJ9.NEvauwy77uvgEvKOlGLmLJJEseamUKhAAPaGaWlD5P5qLHkLiHC9eQOy4YwR+L3BjNN1lumBhg8eEHus23CflQ==")
+        do {
+            let token = try JWToken(payload: message, algorithm: .hs(._512("secret")))
+            XCTAssertEqual(try token.tokenString(),
+                           "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhIjoiYiJ9.NEvauwy77uvgEvKOlGLmLJJEseamUKhAAPaGaWlD5P5qLHkLiHC9eQOy4YwR+L3BjNN1lumBhg8eEHus23CflQ==")
+            XCTAssertTrue(try token.verifySignature(key: "secret"))
+        } catch {
+            XCTFail()
+        }
     }
 
     func testEncodeWithES256Encryption() {
         let privateKey = "AL3BRa7llckPgUw3Si2KCy1kRUZJ/pxJ29nlr86xlm0="
         let publicKey = "BPtT5aOJu133UmfZNr6J0xYifrtknN0sk0VbuB/xqdQHCFpzpWmm8c9HnesXRvu21o37MkzkO6hKxGFNzO73UGc="
 
-        let token = try! encode(message, with: .es(._256(privateKey)))
-        let segments = token.components(separatedBy: ".")
-
-        XCTAssertEqual(segments.count, 3)
-        let derBytes = try! Data(base64Encoded: segments[2])!.makeBytes()
-        var derBytesPointer: UnsafePointer? = UnsafePointer(derBytes)
-
-        let signature = d2i_ECDSA_SIG(nil, &derBytesPointer, derBytes.count)
-
-        let digest = try! Hash(.sha256, "\(segments[0]).\(segments[1])".bytes).hash()
-        let ecKey = try! newECPublicKey(._256(publicKey))
-        let verified = ECDSA_do_verify(digest, Int32(digest.count), signature, ecKey)
-
-        XCTAssertEqual(verified, 1)
+        do {
+            let token = try JWToken(payload: message, algorithm: .es(._256(privateKey)))
+            XCTAssertTrue(try token.verifySignature(key: publicKey))
+        } catch {
+            XCTFail()
+        }
     }
 
     func testEncodeWithES384Encryption() {
         let privateKey = "BVjgIwqB1PeIv0YP3ZeQ7CTqW+9+yJ7Y/jXPr1tfrb3ne5TQNMU2rOxYtf6M6IN9"
         let publicKey = "BAV9NUl1v488eXcDhStPTK57Dg4Cm+XCjmFGD1IXjkF+LJNG963oUkYjV1xxNQgxJka7tsXjadei25PdZatX3GS6KivPiNu1YdeSpXxr5d73sdLU/rq/OgDLUUcic77g3A=="
 
-        let token = try! encode(message, with: .es(._384(privateKey)))
-        let segments = token.components(separatedBy: ".")
-
-        XCTAssertEqual(segments.count, 3)
-        let derBytes = try! Data(base64Encoded: segments[2])!.makeBytes()
-        var derBytesPointer: UnsafePointer? = UnsafePointer(derBytes)
-
-        let signature = d2i_ECDSA_SIG(nil, &derBytesPointer, derBytes.count)
-
-        let digest = try! Hash(.sha384, "\(segments[0]).\(segments[1])".bytes).hash()
-        let ecKey = try! newECPublicKey(._384(publicKey))
-        let verified = ECDSA_do_verify(digest, Int32(digest.count), signature, ecKey)
-
-        XCTAssertEqual(verified, 1)
+        do {
+            let token = try JWToken(payload: message, algorithm: .es(._384(privateKey)))
+            XCTAssertTrue(try token.verifySignature(key: publicKey))
+        } catch {
+            XCTFail()
+        }
     }
 
     func testEncodeWithES512Encryption() {
         let privateKey = "AdcqLUGC+4OlxqGRJkz6++BD9tfDtNM1NvTQh7M0stzvMMvcaNio87YUD6Gaks0eS9Krvs1Bkqo/T7k9DW8Eyoec"
         let publicKey = "BACJKkoouMqhbgZtTSyQJHECDf/V2ArN7VwuaIUIKsx/OFiF79ccrzCSZ1MrGOmQgPAez6pqjUIjhwoHr5tRH65BggEUhC7SNvRvfMeElOrZNac9uTVnfGJ1DywnL+JtD49ytuD9GjifUPHJi4RcN36RHLXyBpF0u1+RRFbKCNhnJ132Pw=="
 
-        let token = try! encode(message, with: .es(._512(privateKey)))
-        let segments = token.components(separatedBy: ".")
-
-        XCTAssertEqual(segments.count, 3)
-        let derBytes = try! Data(base64Encoded: segments[2])!.makeBytes()
-        var derBytesPointer: UnsafePointer? = UnsafePointer(derBytes)
-
-        let signature = d2i_ECDSA_SIG(nil, &derBytesPointer, derBytes.count)
-
-        let digest = try! Hash(.sha512, "\(segments[0]).\(segments[1])".bytes).hash()
-        let ecKey = try! newECPublicKey(._512(publicKey))
-        let verified = ECDSA_do_verify(digest, Int32(digest.count), signature, ecKey)
-
-        XCTAssertEqual(verified, 1)
+        do {
+            let token = try JWToken(payload: message, algorithm: .es(._512(privateKey)))
+            XCTAssertTrue(try token.verifySignature(key: publicKey))
+        } catch {
+            XCTFail()
+        }
     }
 
     func testDecode() {

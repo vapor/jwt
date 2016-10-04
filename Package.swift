ab9ece0e2ef257a523fc4dc6cf37e51b03c449ca
Matthijs Logemann <matthijs.logemann@xs4all.nl>
2016-10-04 16:39:31 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "VaporJWT",
     dependencies: [
         .Package(url: "https://github.com/vapor/Crypto.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/vapor/JSON.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/vapor/json.git", majorVersion: 1, minor: 0),
     ]
 )

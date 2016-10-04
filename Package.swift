fb18142042607c69c70388826050abd8446f3827
Matthijs Logemann <matthijs.logemann@xs4all.nl>
2016-10-04 21:51:52 +0200

    Make name normal

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "vapor-jwt",
+    name: "VaporJWT",
     dependencies: [
         .Package(url: "https://github.com/vapor/crypto.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/json.git", majorVersion: 1),

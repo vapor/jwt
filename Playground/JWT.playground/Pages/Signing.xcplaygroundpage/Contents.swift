88d5f0069f20c858b87e77b496303f87b3f35e6d
Siemen Sikkema <siemen@unclutter.nl>
2017-02-15 17:05:16 +0000

    Update Playgrounds

--- Playground/JWT.playground/Pages/Signing.xcplaygroundpage/Contents.swift
+++ Playground/JWT.playground/Pages/Signing.xcplaygroundpage/Contents.swift
@@ -2,7 +2,6 @@
 //: ## Signing
 import Core
 import JWT
-
 //: `JWT` includes 7 signers:
 //: * HS256
 //: * HS384
@@ -14,24 +13,25 @@ import JWT
 //:
 //: To create your own signer, adapt to the `Signer` protocol.
 struct XOR: Signer {
-
     let key: Byte
 
     init(key: Byte) {
         self.key = key
     }
 
-    func sign(_ message: Bytes) throws -> Bytes {
+    func sign(message: Bytes) throws -> Bytes {
         return message.map { $0 ^ key }
     }
 
-    func verifySignature(_ signature: Bytes, message: Bytes) throws -> Bool {
-        return signature.map { $0 ^ key } == message
+    func verify(signature: Bytes, message: Bytes) throws {
+        guard signature.map({ $0 ^ key }) == message else {
+            throw JWTError.signatureVerificationFailed
+        }
     }
 }
 
 let xor = XOR(key: 87)
 let message = "secret message".bytes
-let signature = try xor.sign(message)
-try xor.verifySignature(signature, message: message)
+let signature = try xor.sign(message: message)
+try xor.verify(signature: signature, message: message)
 //: [Next](@next)

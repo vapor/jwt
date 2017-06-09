33f9ae093621de43141c70c45d107d34694aa7b3
Valerio Mazzeo <valeriomazzeo@users.noreply.github.com>
2017-06-09 17:18:28 +0100

    parameters documentation

--- Sources/JWT/Signers/RSAKey+JWK.swift
+++ Sources/JWT/Signers/RSAKey+JWK.swift
@@ -3,6 +3,11 @@ import CTLS
 
 public extension RSAKey {
 
+    /**
+        - parameter n: Base64 URL encoded string representing the `modulus` of the RSA Key.
+        - parameter e: Base64 URL encoded string representing the `public exponent` of the RSA Key.
+        - parameter d: Base64 URL encoded string representing the `private exponent` of the RSA Key.
+     */
     public init(n: String, e: String, d: String? = nil) throws {
 
         func parseBignum(_ s: String) -> UnsafeMutablePointer<BIGNUM> {
@@ -11,7 +16,10 @@ public extension RSAKey {
             }
         }
 
-        let rsa = RSA_new()!
+        guard let rsa = RSA_new() else {
+            throw JWTError.createKey
+        }
+
         rsa.pointee.n = parseBignum(n)
         rsa.pointee.e = parseBignum(e)
 

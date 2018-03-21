d3695c51ec1b30bed0c7c9d94060378eb179c767
tanner0101 <me@tanner.xyz>
2018-03-21 18:27:09 -0400

    rc.2 updates

--- Package.swift
+++ Package.swift
@@ -8,10 +8,10 @@ let package = Package(
     ],
     dependencies: [
         // 🌎 Utility package containing tools for byte manipulation, Codable, OS APIs, and debugging.
-        .package(url: "https://github.com/vapor/core.git", .branch("master")),
+        .package(url: "https://github.com/vapor/core.git", from: "3.0.0-rc.2"),
 
         // 🔑 Hashing (BCrypt, SHA, HMAC, etc), encryption, and randomness.
-        .package(url: "https://github.com/vapor/crypto.git", .branch("master")),
+        .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0-rc.2"),
     ],
     targets: [
         .target(name: "JWT", dependencies: ["Core", "Crypto"]),

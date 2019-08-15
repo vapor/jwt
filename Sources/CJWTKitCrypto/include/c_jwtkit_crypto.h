639c1ebfd166816c18654a440c7ec85951261ec6
Tanner Nelson <me@tanner.xyz>
2019-08-15 00:19:37 -0400

    remove all shims

--- Sources/CJWTKitCrypto/include/c_jwtkit_crypto.h
+++ Sources/CJWTKitCrypto/include/c_jwtkit_crypto.h
@@ -14,10 +14,9 @@
 #include <openssl/pkcs12.h>
 #include <openssl/x509v3.h>
 
-EVP_MD_CTX *jwtkit_EVP_MD_CTX_new(void);
-void jwtkit_EVP_MD_CTX_free(EVP_MD_CTX *ctx);
-int jwtkit_RSA_set0_key(RSA *r, BIGNUM *n, BIGNUM *e, BIGNUM *d);
-HMAC_CTX *jwtkit_HMAC_CTX_new(void);
-void jwtkit_HMAC_CTX_free(HMAC_CTX *ctx);
+//EVP_MD_CTX *jwtkit_EVP_MD_CTX_new(void);
+//void jwtkit_EVP_MD_CTX_free(EVP_MD_CTX *ctx);
+//HMAC_CTX *jwtkit_HMAC_CTX_new(void);
+//void jwtkit_HMAC_CTX_free(HMAC_CTX *ctx);
 
 #endif

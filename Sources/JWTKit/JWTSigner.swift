7c8a60b36201296891beaaf9b27200c96ff0e6d3
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-11-06 09:33:04 -0600

    Created JWTSigner.verify(_:header:payload:) method

--- Sources/JWTKit/JWTSigner.swift
+++ Sources/JWTKit/JWTSigner.swift
@@ -1,4 +1,4 @@
-import class Foundation.JSONEncoder
+import struct Foundation.Data
 
 /// A JWT signer.
 public final class JWTSigner {
@@ -6,5 +6,18 @@ public final class JWTSigner {
 
     public init(algorithm: JWTAlgorithm) {
         self.algorithm = algorithm
+    }
+}
+
+extension JWTSigner {
+    public func verify<S, H, P>(_ signature: S, header: H, payload: P) throws -> Bool
+        where S: DataProtocol, H: DataProtocol, P: DataProtocol
+    {
+        let message = Array(header) + Array([.period]) + Array(payload)
+        guard let signature = Data(base64Encoded: Data(signature)) else {
+            throw JWTError.malformedToken
+        }
+
+        return try self.algorithm.verify(signature, signs: message)
     }
 }

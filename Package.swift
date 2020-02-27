d22a64806b236401f6e6cc22512496781e01a30d
Gwynne Raskind <gwynne@darkrainfall.org>
2020-02-26 19:09:22 -0600

    Update OS version requirement for compatibility with latest Vapor beta (#115)

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@ import PackageDescription
 let package = Package(
     name: "jwt",
     platforms: [
-       .macOS(.v10_14)
+       .macOS(.v10_15)
     ],
     products: [
         .library(name: "JWT", targets: ["JWT"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0-beta.2.5"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.3.19"),
+        .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0-beta.3"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.4"),
     ],
     targets: [
         .target(name: "JWT", dependencies: ["JWTKit", "Vapor"]),

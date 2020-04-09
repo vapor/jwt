8c1681e9497b72430fa62962f61339d3573c7b0f
Gwynne Raskind <gwynne@darkrainfall.org>
2020-04-09 12:58:51 -0500

    Update to Vapor 4 GM (#119)
    * Update JWT for Vapor GM branch. The tests now additionally verify that the guard middleware works as intended.
    
    * Update CI workflow (#120)
    
    * from 4.0.0 tag
    
    Co-authored-by: tanner0101 <me@tanner.xyz>

--- Sources/JWT/JWTAuthenticator.swift
+++ Sources/JWT/JWTAuthenticator.swift
@@ -2,11 +2,11 @@ import Vapor
 
 public protocol JWTAuthenticator: BearerAuthenticator {
     associatedtype Payload: JWTPayload
-    func authenticate(jwt: Payload, for request: Request) -> EventLoopFuture<User?>
+    func authenticate(jwt: Payload, for request: Request) -> EventLoopFuture<Void>
 }
 
 extension JWTAuthenticator {
-    public func authenticate(bearer: BearerAuthorization, for request: Request) -> EventLoopFuture<User?> {
+    public func authenticate(bearer: BearerAuthorization, for request: Request) -> EventLoopFuture<Void> {
         do {
             return try self.authenticate(
                 jwt: request.jwt.verify([UInt8](bearer.token.utf8)),

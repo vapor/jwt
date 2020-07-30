432a1042693a5d9e643ec4888338b7172f2fa107
Tanner <tannernelson@gmail.com>
2020-07-30 12:17:56 -0400

    Support of Microsoft's OpenId Connect platform (#130)
    Co-authored-by: Marcin Czachurski <mczachurski@icloud.com>

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "JWT", targets: ["JWT"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0-rc.1"),
+        .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
     ],
     targets: [

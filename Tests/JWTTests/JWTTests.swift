ffefaff317a49564911c212f1c8eebd9d84cb0e0
tanner0101 <me@tanner.xyz>
2020-07-30 13:29:36 -0400

    JWT 4.0.0

--- Tests/JWTTests/JWTTests.swift
+++ Tests/JWTTests/JWTTests.swift
@@ -3,6 +3,82 @@ import JWTKit
 import XCTVapor
 
 class JWTTests: XCTestCase {
+    func testDocs() throws {
+        // creates a new application for testing
+        let app = Application(.testing)
+        defer { app.shutdown() }
+
+        // Add HMAC with SHA-256 signer.
+        app.jwt.signers.use(.hs256(key: "secret"))
+
+        // JWT payload structure.
+        struct TestPayload: JWTPayload {
+            // Maps the longer Swift property names to the
+            // shortened keys used in the JWT payload.
+            enum CodingKeys: String, CodingKey {
+                case subject = "sub"
+                case expiration = "exp"
+                case isAdmin = "admin"
+            }
+
+            // The "sub" (subject) claim identifies the principal that is the
+            // subject of the JWT.
+            var subject: SubjectClaim
+
+            // The "exp" (expiration time) claim identifies the expiration time on
+            // or after which the JWT MUST NOT be accepted for processing.
+            var expiration: ExpirationClaim
+
+            // Custom data.
+            // If true, the user is an admin.
+            var isAdmin: Bool
+
+            // Run any additional verification logic beyond
+            // signature verification here.
+            // Since we have an ExpirationClaim, we will
+            // call its verify method.
+            func verify(using signer: JWTSigner) throws {
+                try self.expiration.verifyNotExpired()
+            }
+        }
+
+        // Fetch and verify JWT from incoming request.
+        app.get("me") { req -> HTTPStatus in
+            let payload = try req.jwt.verify(as: TestPayload.self)
+            print(payload)
+            return .ok
+        }
+
+        // Generate and return a new JWT.
+        app.post("login") { req -> [String: String] in
+            // Create a new instance of our JWTPayload
+            let payload = TestPayload(
+                subject: "vapor",
+                expiration: .init(value: .distantFuture),
+                isAdmin: true
+            )
+            // Return the signed JWT
+            return try [
+                "token": req.jwt.sign(payload)
+            ]
+        }
+
+        try app.test(.GET, "me", beforeRequest: { req in
+            req.headers.bearerAuthorization = .init(token: """
+            eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ2YXBvciIsImV4cCI6NjQwOTIyMTEyMDAsImFkbWluIjp0cnVlfQ.lS5lpwfRNSZDvpGQk6x5JI1g40gkYCOWqbc3J_ghowo
+            """)
+            print(req)
+        }, afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+        }).test(.POST, "login", beforeRequest: { req in
+            print(req)
+        }, afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            print(res.body.string)
+            try XCTAssertNotNil(res.content.decode([String: String].self)["token"])
+        })
+    }
+
     // manual authentication using req.jwt.verify
     func testManual() throws {
         // creates a new application for testing

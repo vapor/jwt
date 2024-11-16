ec5a9d489a73560732c7a27ce860fe799b1413be
Tim Condon <0xTim@users.noreply.github.com>
2024-11-16 04:13:06 +0100

    Correctly conform `JWTError` to `AbortError` (#162)
    * Update SIWA test with new token and testing the body so we know when it expires
    
    * Wrap JWTError for conformance
    
    * Wrap more errors
    
    * Better workaround
    
    * Remove unused import
    
    * Fix lint error
    
    * Do it 'correctly'
    
    * Fix linting

--- Sources/JWT/JWTError+Vapor.swift
+++ Sources/JWT/JWTError+Vapor.swift
@@ -4,4 +4,8 @@ extension JWTError: @retroactive AbortError {
     public var status: HTTPResponseStatus {
         .unauthorized
     }
+
+    @_implements(AbortError,reason) public var abortErrorReason: String {
+        self.reason ?? self.description
+    }
 }
